b2c7925d26bc1f622e4cdb961630e95d4d1ebef6
Jafar Kasymov <116582499+JafarKasimov@users.noreply.github.com>
2023-06-08 16:01:09 +0600

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,9 @@
+//Top - level build file where you can add configuration options common to all sub - projects / modules.plugins {
+plugins {
+    id(Plugins.application) version Versions.application apply false
+    id(Plugins.androidLibrary) version Versions.library apply false
+    kotlin(Plugins.kotlinAndroid) version Versions.kotlinAndroid apply false
+    id(Plugins.hilt) version Versions.hilt apply false
+    id(Plugins.safeArgs) version Versions.safeArgs apply false
+    id(Plugins.kotlinJvm) version Versions.kotlinJvm apply false
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "Android_5__Lesson6"
+include (":app")
+include(":data")
+include(":domain")
+include(":presentation")
